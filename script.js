77f9dfd141021b7212bb2af41e031d3c5e942c1a
nate1789 <102476136+nate1789@users.noreply.github.com>
2024-05-03 17:16:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingNextValue = false;
+
 function sendNumberValue(number) {
     // If current display value is 0, replace it, if not add number
     const displayValue = calculatorDisplay.textContent;
@@ -9,13 +13,42 @@ function sendNumberValue(number) {
     displayValue + number;
 }
 
+function addDecimal() {
+	// If no decimal, add one
+	if (!calculatorDisplay.textContent.includes('.')) {
+		calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`
+	}
+}
+
+function useOperator(operator) {
+	const currentValue = Number(calculatorDisplay.textContent);
+	// Assign firstvalue if no value
+	if (!firstValue) {
+		firstValue = currentValue;
+	}
+	operatorValue = operator;
+	console.log('firstValue', firstValue);
+	console.log('operator', operatorValue);
+}
+
 // Add Event Listeners for numbers, operators, and decimal buttons
 inputBtns.forEach((inputBtn) => {
     if (inputBtn.classList.length === 0) {
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if (inputBtn.classList.contains('operator')) {
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
     } else if (inputBtn.classList.contains('decimal')) {
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => addDecimal());
     }
 });
+
+// Reset all values, display
+function resetAll() {
+	firstValue = 0;
+	operatorValue = '';
+	awaitingNextValue = false;
+	calculatorDisplay.textContent = '0';
+}
+
+// Event Listeners
+clearBtn.addEventListener('click', resetAll);
